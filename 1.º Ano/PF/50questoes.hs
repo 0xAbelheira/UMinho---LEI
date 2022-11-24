0301bc528d9d878265fbce5e380034fdbfb8ac04
0xAbelheira <h.abelheira@gmail.com>
2022-11-24 11:02:05 +0000

    Enviado ficheiro errado

--- 1.º Ano/PF/50questoes.hs
+++ 1.º Ano/PF/50questoes.hs
@@ -1,192 +1,357 @@
-import 
+module Questoes where
+
+import Data.List
+
+--exercício 1
 
 enumFromTo' :: Int -> Int -> [Int]
-enumFromTo' a b | a == b = [b]
-                | otherwise = a : enumFromTo' (a+1) b
+enumFromTo' start end 
+    | start == end = [end]
+    | otherwise = start:enumFromTo' (start+1) end
 
-enumFromThenTo' :: Int -> Int -> Int -> [Int]
-enumFromThenTo' x y z
-               | x > z && (y-x) > 0 = []
-               | x < z && (y-x) < 0 = []
-               | otherwise = (x : enumFromThenTo' y (y + (y - x)) z)
+--exercício 2
 
-concatena :: [a] -> [a] -> [a]
-concatena [] [] = []
-concatena [] l2 = l2
-concatena (x:xs) l = x : concatena xs l
+enumFromThenTo' :: Int -> Int -> Int ->[Int]
+enumFromThenTo' start next end
+    | start > end = []
+    | otherwise = start:enumFromThenTo' next (2 * next - start) end
 
-encontra :: [a] -> Int -> a
-encontra (x:xs) a | a == 0 = x 
-                  | otherwise = encontra xs (a - 1)
+--exercício 3
+
+addadd' :: [a] -> [a] -> [a]
+addadd' [] [] = []
+addadd' [] y = y
+addadd' (x:xs) y = x:addadd' xs y
+
+--exercício 4
+
+exclamation' :: [a] -> Int -> a
+exclamation' (x:xs) y 
+    | y == 0 = x
+    | otherwise = exclamation' xs (y-1)
+
+--exercício 5
 
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
+--exercício 6
+
 take' :: Int -> [a] -> [a]
 take' _ [] = []
-take' 0 l = []
-take' n (x:xs) = x : take' (n-1) xs
+take' 0 n = []
+take' n (x:xs) = x:take' (n-1) xs 
+
+--exercício 7
 
 drop' :: Int -> [a] -> [a]
-drop' 0 l = l
 drop' _ [] = []
+drop' 0 n = []
 drop' n (x:xs) = drop' (n-1) xs
+--exercício 8
 
 zip' :: [a] -> [b] -> [(a,b)]
-zip' [] _ = []
 zip' _ [] = []
-zip' (x:xs) (y:ys) = (x,y) : zip' xs ys 
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+--exercício 9
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' n (x:xs) = n == x || elem' n xs
+
+--exercício 10
 
 replicate' :: Int -> a -> [a]
 replicate' 0 _ = []
-replicate' n x = x : replicate' (n-1) x
+replicate' n x = x:replicate' (n-1) x
 
-intersperse :: a -> [a] -> [a]
-intersperse _ [] = []
-intersperse e [x] = [x]
-intersperse e (x:xs) = x : e : intersperse e xs
+--exercício 11
 
---group' :: Eq a => [a] -> [[a]]
---group' [] = []
---group' (x:xs) = (x : takeWhile (==x) xs) : group' (dropWhile (==x) xs)
+intersperse' :: a -> [a] -> [a]
+intersperse' _ [] = []
+intersperse' _ [a] = [a]
+intersperse' a (x:xs) = x:a:intersperse' a xs
+
+
+--exercício 12
+
+group' :: Eq a => [a] -> [[a]]
+group' [] = []
+group' (x:xs) = (x:takeWhile(== x) xs):group' (dropWhile(==x) xs)
+
+--exercício 13
 
 concat' :: [[a]] -> [a]
 concat' [] = []
 concat' (x:xs) = x ++ concat' xs
 
-inits :: [a] -> [[a]]
-inits [] = [[]]
-inits l = inits (init l) ++ [l]
+--exercício 14
 
-tails :: [a] -> [[a]]
-tails [] = [[]]
-tails l = [l] ++ tails (tail l) 
+inits' :: [a] -> [[a]]
+inits' [] = [[]]
+inits' l = inits' (init l) ++ [l]
 
-heads :: [[a]] -> [a]
-heads [] = []
-heads ([]:t) = heads t
-heads (x:xs) = head x : heads xs
+--exercício 15
 
-total :: [[a]] -> Int
-total [] = 0
-total (x:xs) = length x + total xs
+tails' :: [a] -> [[a]]
+tails' [] = [[]]
+tails' l = l:tails' (tail l) 
+--exercício 16
 
-fun :: [(a,b,c)] -> [(a,c)]
-fun [] = []
-fun ((a,b,c):xs) = [(a,c)] ++ fun xs
+isPrefixOf' :: Eq a => [a] -> [a] -> Bool
+isPrefixOf' _ [] = False
+isPrefixOf' [] _ = True
+isPrefixOf' (x:xs) (y:ys) = x == y && isPrefixOf' xs ys
 
-cola :: [(String,b,c)] -> String
-cola [] = ""
-cola ((x,a,b):xs) = x ++ cola xs
+--exercício 17 
 
-idade :: Int -> Int -> [(String,Int)] -> [String]
-idade _ _ [] = []
-idade a1 i ((x,a2):xs) = if ((a1 - a2) >= i) then [x] ++ idade a1 i xs
-                                             else idade a1 i xs
+isSuffixOf' :: Eq a => [a] -> [a] -> Bool
+isSuffixOf' _ [] = True
+isSuffixOf' [] _ = False
+isSuffixOf' l ls@(_:t) = l == ls || isSuffixOf' l t 
 
-powerEnumFrom :: Int -> Int -> [Int]
-powerEnumFrom n 1 = [1]
-powerEnumFrom n m | m > 1 = powerEnumFrom n (m-1) ++ [n^(m-1)]
-                  | otherwise = []
+{-@ significa "também se chama por" logo diz que ls é uma lista que acrescenta um sufixo-}
 
-isPrime :: Int -> Bool
-isPrime n 
-    | n >= 2 = primeCheck n 2
-    | otherwise = False 
+--exercício 18
 
-primeCheck :: Int -> Int -> Bool
-primeCheck n m 
-    | m * m > n = True
-    | mod n m == 0 = False 
-    | otherwise = primeCheck n (m+1)      
+isSubsequenceOf' :: Eq a => [a] -> [a] -> Bool
+isSubsequenceOf' [] _ = True
+isSubsequenceOf' _ [] = False
+isSubsequenceOf' (x:xs) (y:ys) = x == y && isSubsequenceOf' xs ys || isSubsequenceOf' (x:xs) ys
 
+--exercício 19
 
-isPrefixOf :: Eq a => [a] -> [a] -> Bool
-isPrefixOf [] _ = True
-isPrefixOf _ [] = False 
-isPrefixOf (x:xs) (y:ys) = if x == y && isPrefixOf xs ys then True else False
+elemIndices' :: Eq a => a -> [a] -> [Int]
+elemIndices' _ [] = []
+elemIndices' a (x:xs) | a == x = 0:map (+1) (elemIndices' a xs)
+                      | otherwise = map (+1) (elemIndices' a xs)
 
-isSuffixOf :: Eq a => [a] -> [a] -> Bool 
-isSuffixOf [] _ = True
-isSuffixOf _ [] = False 
-isSuffixOf l (x:xs) = l == (x:xs) || isSuffixOf l xs
+--exercício 20
 
+nub' :: Eq a => [a] -> [a]
+nub' [] = []
+nub' (x:xs) = x:filter (/=x) (nub' xs)
 
-isSubsequenceOf :: Eq a => [a] -> [a] -> Bool
-isSubsequenceOf [] _ = True
-isSubsequenceOf _ [] = False
-isSubsequenceOf (x:xs) (y:ys) = if x == y && isSubsequenceOf xs ys || isSubsequenceOf (x:xs) ys then True else False
-
-elemIndices :: Eq a => a -> [a] -> [Int]
-elemIndices x l = elemIndicesAux x l 0
-
-elemIndicesAux :: Eq a => a -> [a] -> Int -> [Int]
-elemIndicesAux _ [] _ = []
-elemIndicesAux x (h:t) i 
-    | x == h = i : elemIndicesAux x t (i+1)
-    | otherwise = elemIndicesAux x t (i+1)
-
-nub :: Eq a => [a] -> [a]
-nub [] = []
-nub (x:xs) = x : filter (/= x) (nub xs)
+--exercício 21
 
 delete' :: Eq a => a -> [a] -> [a]
 delete' _ [] = []
-delete' n (x:xs) | n == x = xs
-                 | otherwise = x : delete' n xs 
+delete' a (x:xs) | a == x = xs
+                 | otherwise = x:delete' a xs
 
-remove' :: Eq a => [a] -> [a] -> [a]
-remove' [] _ = []
-remove' l1 [] = l1 
-remove' l1 (x:xs) = remove' (delete' x l1) xs    
+--exercício 22
+
+slash' :: Eq a => [a] -> [a] -> [a]
+slash' l [] = l
+slash' _ [] = []
+slash' l (x:xs) = slash' (delete' x l) xs
+
+--exercício 23
 
 union' :: Eq a => [a] -> [a] -> [a]
-union' l1 [] = l1
-union' l (x:xs) | elem x l = union' l xs 
-                | otherwise = union' (l ++ [x]) xs 
+union' l [] = l
+union' l (x:xs) | elem x l = union' l xs
+                | otherwise = union' (l ++ [x]) xs
 
-intersect :: Eq a => [a] -> [a] -> [a]
-intersect _ [] = []
-intersect (x:xs) l | elem x l = x : intersect l xs
-                   | otherwise = intersect l xs
+--exercício 24
+
+intersect' :: Eq a => [a] -> [a] -> [a]
+intersect' [] _ = []
+intersect' (x:xs) l | elem x l = x:intersect' xs l
+                    | otherwise = intersect' xs l
+
+--exercício 25
 
 insert' :: Ord a => a -> [a] -> [a]
-insert' n [] = [n]
-insert' n (x:xs) | n < x = n : x : xs
-                 | otherwise = x : insert' n xs
+insert' a [] = [a]
+insert' a (x:xs) | a > x = x: insert' a xs
+                 | otherwise = a:x:xs
+
+--exercício 26
 
 unwords' :: [String] -> String
 unwords' [] = ""
-unwords' (x:xs) = x ++ " " ++ unwords' xs
+unwords' (x:xs) = x ++ (if xs == [] then "" else " ") ++ unwords' xs
+
+{-o segundo "" tem um espaço dentro para as strings não ficarem juntas-}
+
+--exercício 27
 
 unlines' :: [String] -> String
 unlines' [] = ""
 unlines' (x:xs) = x ++ "\n" ++ unlines' xs
 
---pMaior :: Ord a => [a] -> Int
---pMaior a =
+--exercício 28
 
---iSort :: Ord a => [a] -> [a]
---iSort (x:xs) = insert x (iSort xs)
+pMaior :: Ord a => [a] -> Int
+pMaior [a] = 0
+pMaior (x:xs) | x > (xs !! l) = 0
+              | otherwise = 1 + l
+              where l = pMaior xs
 
+{-(!!) função predefinida anteriormente no exercício 4-}
 
+--exercício 29
 
+temRepetidos :: Eq a => [a] -> Bool
+temRepetidos [] = False
+temRepetidos (x:xs) = elem x xs || temRepetidos xs
 
+--exercício 30
 
+algarismos :: [Char] -> [Char]
+algarismos [] = []
+algarismos (x:xs) | elem x  ['0'..'9'] = x:algarismos xs
+                  | otherwise = algarismos xs
 
+--exercício 31
 
+posImpares :: [a] -> [a]
+posImpares [] = []
+posImpares [c] = []
+posImpares (h:x:xs) = x:posImpares xs
 
+--exercício 32
 
+posPares :: [a] -> [a]
+posPares [] = []
+posPares [c] = []
+posPares (h:x:xs) = h:posPares xs
 
+--exercício 33
 
+isSorted :: Ord a => [a] -> Bool
+isSorted [] = True
+isSorted [c] = True
+isSorted (h:x:xs) = x >= h && isSorted (x:xs)
 
+--exercício 34
 
+iSort :: Ord a => [a] -> [a]
+iSort [] = []
+iSort (x:xs) = insert x (iSort xs)
 
+--exercício 35
 
+menor :: String -> String -> Bool
+menor _ "" = False
+menor "" _ = True
+menor (x:xs) (y:ys) = x < y || menor xs ys
 
+--exercício 36
 
+elemMSet :: Eq a => a -> [(a,Int)] -> Bool
+elemMSet a [] = False
+elemMSet a ((x,n):xs) = a == x || elemMSet a xs
 
+--exercício 37
 
+lenghtMSet :: [(a,Int)] -> Int
+ĺenghtMSet [] = 0
+lenghtMSet ((x,n):xs) = n + lenghtMSet xs
 
+--exercício 38
 
+converteMSet :: [(a,Int)] -> [a]
+converteMSet [] = []
+converteMSet ((x,n):xs) = replicate n x ++ converteMSet xs
+
+--exercício 39
+
+insereMSet :: Eq a => a -> [(a,Int)] -> [(a,Int)]
+insereMSet a [] = [(a,1)]
+insereMSet a ((x,n):xs) = if a == x then (x,n+1):xs else (a,n):insereMSet a xs
+
+{-estava a dar-me erro a fazer com "|" então fiz com if's-}
+
+--exercício 40
+
+removeMSet :: Eq a => a -> [(a,Int)] -> [(a,Int)]
+removeMSet x [] = []
+removeMSet a ((x,n):xs) = if a == x then xs else (x,n):removeMSet a xs
+
+--exercício 41
+
+constroiMSet :: Ord a => [a] -> [(a,Int)]
+constroiMSet [] = []
+constroiMSet (l:ls) = insereMSet l (constroiMSet ls)
+
+--exercício 42
+
+partitionEithers' :: [Either a b] -> ([a],[b])
+partitionEithers' [] = ([],[])
+partitionEithers' ((Left a):xs) = (a:as,bs)
+    where (as,bs) = partitionEithers' xs
+partitionEithers' ((Right b):xs) = (as,b:bs)
+    where (as,bs) = partitionEithers' xs  
+
+{-ainda não demos isso definitivamente mas se forem ver ao Prelude a função partition EIthers chegais lá-}
+
+--exercício 43
+
+catMaybes' :: [Maybe a] -> [a]
+catMaybes' [] = []
+catMaybes' ((Just x):xs) = x:catMaybes' xs
+catMaybes' (Nothing:xs) = catMaybes' xs
+
+--exercício 44
+
+data Movimento = Norte | Sul | Este | Oeste deriving Show
+
+posicao :: (Int,Int) -> [Movimento] -> (Int,Int)
+posicao p [] = p
+posicao (x, y) (m1:m2) = posicao (case m1 of Norte -> (x, y+1)
+                                             Sul -> (x, y-1)
+                                             Este -> (x+1, y)
+                                             Oeste -> (x-1, y)) m2
+
+--exercício 45
+
+caminho :: (Int,Int) -> (Int,Int) -> [Movimento]
+caminho (xi,yi) (xf,yf) | yi < yf = Norte:caminho (xi, yi + 1) (xf, yf)
+                        | yi > yf = Sul:caminho (xi, yi - 1) (xf, yf)
+                        | xi < xf = Este:caminho (xi + 1, yi) (xf, yf)
+                        | xi > xf = Oeste:caminho (xi - 1, yi) (xf, yf)
+                        | otherwise = []
+
+--exercício 46
+
+vertical :: [Movimento] -> Bool 
+vertical [] = True
+certical (l:ls) = case l of Este -> False
+                            Oeste -> False
+                            otherwise -> vertical ls
+
+--exercício 47
+
+data Posicao = Pos Int Int deriving Show
+
+maisCentral :: [Posicao] -> Posicao
+maisCentral = foldl1 (\(Pos xa ya) (Pos x y) -> if (xa^2 + ya^2) > (x^2 + y^2) then (Pos x y) else (Pos xa ya))
+
+{-função foldl1 é a função foldl(fold à esquerda, l de left) mas só do 1 par-}
+
+--exercício 48
+
+vizinhos :: Posicao -> [Posicao] -> [Posicao]
+vizinhos (Pos x y) ps = filter (\(Pos x1 y1) -> (abs(x1-x) + abs(y1-y) ==1)) ps
+
+--exercício 49
+
+mesmaOrdenada :: [Posicao] -> Bool
+mesmaOrdenada [(Pos x y)] = True
+mesmaOrdenada ((Pos x y):(Pos x1 y1):ps) = y == y1 && mesmaOrdenada ((Pos x1 y1):ps)
+
+--exercício 50
+
+data Semaforo = Verde | Amarelo | Vermelho deriving Show
+
+interseccaoOK :: [Semaforo] -> Bool
+interseccaoOK ss = length [s | s <- ss, case s of Vermelho -> False; otherwise -> True] < 2
+
+{-diz que só existe 2 semáforos (lenght <2) mas que apesar de ter a mesma variável s 
+são diferentes e em caso de o primeiro ser vermelho então não pode passar se for o segundo
+já está autorizado a passar. -}
